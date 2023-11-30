69f93433a90f369d739712dc86e813c25be8c50f
Rafael Galdino <88910477+Rafael-GSousa@users.noreply.github.com>
2023-11-29 21:49:14 -0300

    Atualizando exercicios

--- Exercicios/Aula 01/poo/exercicio-02.js
+++ Exercicios/Aula 01/poo/exercicio-02.js
@@ -8,27 +8,21 @@ class Animal {
 }
 
 class Mamifero extends Animal {
-  constructor(nome) {
-    super(nome, "Mamífero");
-  }
-
   amamentar() {
     return `${this.nome} está amamentando!`;
   }
 }
 
 class Ave extends Animal {
-  constructor(nome) {
-    super(nome, "Ave");
-  }
-
   voar() {
     return `${this.nome} está voando!`;
   }
 }
 
-const animal01 = new Mamifero("Doguinho");
-const animal02 = new Ave("Jubileu");
+const animal01 = new Mamifero("Doguinho", "Mamifero");
+const animal02 = new Ave("Jubileu", 'Ave');
 
+console.log(animal01);
+console.log(animal02);
 console.log(animal01.amamentar());
 console.log(animal02.voar());
